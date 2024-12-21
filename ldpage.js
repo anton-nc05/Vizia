7263b79ee9638ca6f4038d2062b7a0c1e32d4453
anton-nc05 <150932043+anton-nc05@users.noreply.github.com>
2024-12-20 23:14:55 -0500

    js backend revamp

--- ldpage.js
+++
@@ -1,9 +0,0 @@
-const searchInput = document.getElementById("search");
-const buttons = document.querySelectorAll(".grid button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    // Append to search input
-    searchInput.value += button.textContent.trim();
-  });
-});
